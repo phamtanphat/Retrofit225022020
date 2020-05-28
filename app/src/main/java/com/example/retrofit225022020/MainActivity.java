cf101cc746689b2abf97f82bd87adf516122f598
phatdroid94 <phatdroid94@gmail.com>
2020-05-28 20:05:17 +0700

    glide

--- app/src/main/java/com/example/retrofit225022020/MainActivity.java
+++ app/src/main/java/com/example/retrofit225022020/MainActivity.java
@@ -3,7 +3,14 @@ package com.example.retrofit225022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.EncodeStrategy;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -12,28 +19,35 @@ import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.Retrofit.Builder;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView mImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImg = findViewById(R.id.imageview);
 
         // Cach su dung lib retrofit
         // 1 : Khởi tạo ra retrofit
 
         // OkHttpClient : Config các connection
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                                    .readTimeout(30, TimeUnit.SECONDS)
-                                    .connectTimeout(30, TimeUnit.SECONDS)
-                                    .writeTimeout(30, TimeUnit.SECONDS)
-                                    .retryOnConnectionFailure(true)
-                                    .protocols(Arrays.asList(Protocol.HTTP_1_1))
-                                    .build();
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                .build();
 
         // Gson : parse dư liệu về dạng object của java
         Gson gson = new GsonBuilder()
@@ -41,12 +55,32 @@ public class MainActivity extends AppCompatActivity {
                 .disableHtmlEscaping()
                 .create();
         Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl("https://khoapham.vn/KhoaPhamTraining/json/tien/")
-                            .client(okHttpClient)
-                            .addConverterFactory()
-
+                .baseUrl("https://khoapham.vn/KhoaPhamTraining/json/tien/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
         // 2 : Định nghĩa các request call http
         // 3 : Gọi request muốn thực thi
+        ApiRequest apiRequest = retrofit.create(ApiRequest.class);
+
+        Call<Demo1> demo1Call = apiRequest.callDemo1();
+        demo1Call.enqueue(new Callback<Demo1>() {
+            @Override
+            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+                Demo1 demo1 = response.body();
+                Glide.with(MainActivity.this)
+                        .load(demo1.getLogo())
+                        .placeholder(R.mipmap.ic_launcher)
+                        .error(R.drawable.ic_launcher_background)
+                        .into(mImg);
+            }
+
+            @Override
+            public void onFailure(Call<Demo1> call, Throwable t) {
+
+            }
+        });
+
         // 4 : Nhận dữ liệu từ request thông qua phương thức Call
 
     }
